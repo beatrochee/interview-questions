0f0656e1ca1f916606ff030d47ec62fc327ea252
James Butler <jambutler@groupon.com>
2019-08-16 12:37:22 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Responsive } from './responsive/responsive'
+import { BrowserCompat } from './browser-compat/browser-compat';
 
 function BasicExample() {
   return (
@@ -11,7 +12,7 @@ function BasicExample() {
             <Link to="/">Responsive</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/browser-compat">Browser Compat</Link>
           </li>
           <li>
             <Link to="/topics">Topics</Link>
@@ -21,61 +22,9 @@ function BasicExample() {
         <hr />
 
         <Route exact path="/" component={Responsive} />
-        <Route path="/about" component={About} />
-        <Route path="/topics" component={Topics} />
+        <Route path="/browser-compat" component={BrowserCompat} />
       </div>
     </Router>
   );
 }
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Topics({ match }) {
-  return (
-    <div>
-      <h2>Topics</h2>
-      <ul>
-        <li>
-          <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-        </li>
-      </ul>
-
-      <Route path={`${match.path}/:topicId`} component={Topic} />
-      <Route
-        exact
-        path={match.path}
-        render={() => <h3>Please select a topic.</h3>}
-      />
-    </div>
-  );
-}
-
-function Topic({ match }) {
-  return (
-    <div>
-      <h3>{match.params.topicId}</h3>
-    </div>
-  );
-}
-
 export default BasicExample;
---
+++ src/browser-compat/browser-compat.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./browser-compat.css";
+
+export function BrowserCompat() {
+  return <a href="https://wingardium-leviosa.netlify.com/">Wingardium Leviosa</a>
+}
+
